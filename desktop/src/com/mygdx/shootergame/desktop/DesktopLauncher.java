e2aa00725db2420f71f76ab3ac1e2e4839735d22
MrMichalski <mateusz.michalski2004@gmail.com>
2022-02-12 13:41:15 +0100

    init project FIX

--- desktop/src/com/mygdx/shootergame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/shootergame/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.mygdx.shootergame.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
 		new LwjglApplication(new Main(), config);
 	}
 }
